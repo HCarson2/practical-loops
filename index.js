9e58c56f469a9df1edb8f43bd6218c92da5f3863
HaroldCarson <mair0610@gmail.com>
2025-01-06 21:35:15 -0500

    arbitrary number

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //Part 1
+/*
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("fizzbuzz");
@@ -10,8 +11,17 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+*/
 //Part 2
 //Function to check if a number is prime
 function isPrime(num) {
     if (num <= 1) return false;
+    for  (let i = 2; i <= Math.sqrt(num); i++){
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
+//Arbitrary number (n)
+let n = 4;
